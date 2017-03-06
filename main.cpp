ec3fc6e48653ee724ae52a6e9ba7005c3ee04cad
15327 <bulat15g@gmail.com>
2017-03-06 18:54:29 +0300

    _

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <vector>
-#include "prog.h"
+#include <fstream>
 #define N 10
+#include "prog.h"
 using namespace std;
-
+vector<string>variables;
+/**
+ * заполняет вектор и возвращает его
+ * @return
+ */
 vector <Q> fillprog(){
     vector<Q> qbuff;Q buff;
 
@@ -18,6 +23,10 @@ vector <Q> fillprog(){
     return qbuff;
 }
 
+/**
+ * показывает содержимое вектора
+ * @param test
+ */
 void show(vector<Q> test){
     for (int var = 0; var < test.size(); ++var) {
         cout<<"Q"<<test[var].i<<" "<<test[var].if_char<<"-> Q"<<test[var].to_i;
@@ -25,6 +34,10 @@ void show(vector<Q> test){
     }
 }
 
+/**
+ * заполнение вхоного слова
+ * @return
+ */
 string fillinput(){
     cout<<"введите слово\n";
     string ci;
@@ -36,7 +49,15 @@ string fillinput(){
     return ret;
 }
 
-string check(string input,vector<Q>buff,char alpabet[N]){
+/**
+ * проверка слова на корректность
+ * @param input
+ * @param buff
+ * @param alpabet
+ * @param variable
+ * @return
+ */
+string check(string input,vector<Q>buff,char alpabet[N],vector <string> *variable ){
 
     bool is_correct= false;
     while(!is_correct) {
@@ -53,9 +74,87 @@ string check(string input,vector<Q>buff,char alpabet[N]){
             }
         }
     }
+
+    for (int i = 0; i < buff.size(); ++i) {
+//        if buff[i].
+    }
+
     cout<<"проверка проведена\n";
     return input;
 }
+
+/**
+ * чтение фаила
+ * @param f
+ * @param buff
+ */
+vector <Q> read(){
+    vector<Q> buff;
+    ifstream fin("input");
+    if(fin.fail()){
+        cout<<"фаил не найден,идите учитсья\n";
+        return buff;
+    }
+    while(!fin.eof()){
+        Q test;
+        fin>>test.i>>test.if_char>>test.to_i>>test.to_char>>test.position;
+        buff.push_back(test);
+    }
+    buff.pop_back();
+    return buff;
+}
+
+/**
+ * запись программы,работающей в фаил
+ * запись фаила
+ * @param f
+ * @param buff
+ */
+void out(vector<Q>buff){
+    ofstream fout("output_to_read", ios_base::out | ios_base::trunc);
+    for (int var = 0; var < buff.size(); ++var) {
+        fout<<"Q"<<buff[var].i<<" "<<buff[var].if_char<<"-> Q"<<buff[var].to_i;
+        fout<<" "<<buff[var].to_char<<" ,"<<buff[var].position<<"\n";
+    }
+    cout<<"фаил для чтения успешно записан\n";fout.close();
+    fout.open("output_to_prog", ios_base::out | ios_base::trunc);
+    if(fout.fail())cout<<"efwefewfwefb";
+    for (int var = 0; var < buff.size(); ++var) {
+        fout<<buff[var].i<<" "<<buff[var].if_char<<" "<<buff[var].to_i;
+        fout<<" "<<buff[var].to_char<<" "<<buff[var].position<<"\n";
+    }
+    cout<<"фаил успешно записан\n";
+    fout.close();
+}
+
+/**
+ * диалог с порльзователем
+ * @param base
+ * @param input
+ * @param alpabet
+ */
+void dialogue(vector<Q> *program,string *input,char alpabet[N],string *output){
+    cout<<"выберите одно (a-read&start prog) (b-write&start prog):-  ";
+    char a;cin>>a;
+    switch (a){
+        case 'a':{
+            *program=fillprog();
+            show(*program);
+            *input=fillinput();
+            *input=check(*input,*program,alpabet,&variables);
+            *output=myMT(*program,*input);
+            out(*program);
+            break;
+        }
+        case 'b':{
+            *program=read();
+            show(*program);
+            out(*program);
+            break;
+        }
+
+    }
+};
 
 int main()
 {
@@ -63,14 +162,9 @@ int main()
     char alpabet[N]={'a','b','c','#','_',' ','*','l','r','s'};
     vector<Q> program;
 
-
-    program=fillprog();
-    show(program);
-    input=fillinput();
-    input=check(input,program,alpabet);
+    dialogue(&program,&input,alpabet,&output);
 
 
-    output=myMT(program,input);
 
 
     return 0;
--- prog.h
+++ prog.h
@@ -2,6 +2,11 @@
 #include <vector>
 using namespace std;
 
+/**
+ * Машина тьюринга
+ * оформление переменных -@@любаяпеременная
+ *                       -@а_область видимости
+ */
 
 struct Q{
     string i;
@@ -11,6 +16,13 @@ struct Q{
     char position;//L left,S s,R right
 };
 
+/**
+ * поиск индекса команды из базы
+ * @param base база
+ * @param cond текущее состояние
+ * @param find текущий символ
+ * @return номер команды
+ */
 int find_command(vector<Q>base,string cond,char find){
     for (int i = 0; i < base.size(); ++i) {
         if(base.at(i).i==cond&&(find==(char)base.at(i).if_char))return i;
@@ -19,6 +31,13 @@ int find_command(vector<Q>base,string cond,char find){
     return -1;
 }
 
+/**
+ * определяет,куда сдвигаться при этой команде,параметры такие как у предыдущей команды
+ * @param base
+ * @param tek_index
+ * @param tek_command
+ * @return
+ */
 int move(vector<Q> base, int tek_index, int tek_command) {
     switch (base.at(tek_command).position){
         case 'L': {
@@ -52,6 +71,12 @@ int move(vector<Q> base, int tek_index, int tek_command) {
     return tek_index;
 }
 
+/**
+ * сам процесс работы программы
+ * @param base
+ * @param input
+ * @return обработанное "слово"
+ */
 string myMT(vector<Q>base,string input){
     int tek_index=0,tek_command;
     string tek_condition="0";
@@ -75,6 +100,7 @@ string myMT(vector<Q>base,string input){
 
 
     cout<<"программа успешно завершилась,результат:"<<endl<<input;
+    return input;
 }
 
 
